27bf72ba021aa9bbda6d4f6638cd9e997dbbb024
havenf <havenfricke@u.boisestate.edu>
2024-11-11 10:24:26 -0700

    switch to createBrowserRouter

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,10 @@
 import { App } from './App.jsx';
 import ErrorPage from './pages/ErrorPage.jsx';
 import HomePage from './pages/HomePage.jsx';
-import { createHashRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import StuffPage from './Pages/StuffPage.jsx';
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
